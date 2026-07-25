0e5d786facabcd2ace18b6e4df4e3172b57b9505
avinabgyawali6-design <avinabgyawali6@gmail.com>
2026-07-25 17:02:06 +0545

    aaa

--- MY CAPSTONE PROJECT/script.js
+++ MY CAPSTONE PROJECT/script.js
@@ -1,8 +1,29 @@
 const image=[
-    "SCH.1.jpeg","SCH.2.jpeg","SCHOOL.jpeg"
+    'SCH.1.jpeg','SCH.2.jpeg','SCHOOL.jpeg'
 ];
 
 let current=0;
 
 const slidering=document.getElementById("slider-img");
+
+function showslide(index)
+{
+    slidering.src=image[index];
+}
+
+function prevslide(){
+    current = (current - 1 + image.length) % image.length;
+    showslide(current)
+}
+
+function nextslide() {
+    current= (current + 1) % image.length;
+    showslide(current)
+}
+
+setInterval(nextslide,2000);
+
+
+
+
 
